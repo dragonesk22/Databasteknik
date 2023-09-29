4eb4228e2f00fa476cbcb049e7600a36a6f6e439
Axel Hallsenius <axelhallsenius@gmail.com>
2023-09-29 16:26:22 +0200

    placed an order

--- table_creation_scripts/example_customer.sql
+++ table_creation_scripts/example_customer.sql
@@ -1,5 +1,8 @@
 insert into Customer
 values
-(0,"dude@dudemail.com", "Camping Van 12", 1337, "rugsftw","Lebowski",1337,FALSE)
-(2,"walther@dudemail.com", "Dude Avenue 12", 123456, "bowling123","Walther",121212,TRUE)
+(0,"dude@dudemail.com", "Camping Van 12", 1337, "rugsftw","Lebowski",1337,FALSE),
+(2,"walther@dudemail.com", "Dude Avenue 12", 123456, "bowling123","Walther",121212,TRUE);
 
+insert into Placed_Order
+values
+(12, CURRENT_TIME, NULL, 2);
