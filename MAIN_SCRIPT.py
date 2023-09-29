1657ebe15ce7ccac1e68394249445441f286a0e1
Juan Rodriguez <42043819+dragonesk22@users.noreply.github.com>
2023-09-29 16:45:43 +0200

    reviewed.sql

--- MAIN_SCRIPT.py
+++ MAIN_SCRIPT.py
@@ -3,14 +3,21 @@ from mysql.connector import connect
 db = connect(
     host='fries.it.uu.se',
     user='ht23_1_group_13',
-    password='pasSWd_13'
+    password='pasSWd_13',
+    database='ht23_1_project_group_13'
 )
 
+
 cursor = db.cursor()
+#cursor.execute("SHOW DATABASES")
+
+#for x in cursor:
+#    print(x)
 
 
 table_name = "Customer"
 query = f"SELECT * FROM {table_name}"
+
 cursor.execute(query)
 for row in cursor.fetchall():
     print(row)
