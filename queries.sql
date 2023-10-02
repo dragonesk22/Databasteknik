b072562e80acb18f2a6141e19f5a29e6b16b752b
nstates2 <109498023+nstates2@users.noreply.github.com>
2023-10-02 19:29:45 +0200

    partial completion of sql queries

---
+++ queries.sql
@@ -0,0 +1,50 @@
+# Query to grab welcome text from homepage
+SELECT descr
+from Department
+where dep_ID="1000";
+
+#Query to get top level departments with relevant information for the homepage : title, short desc, link
+SELECT
+    title, descr, link
+FROM
+    Department
+WHERE
+    dep_ID IN (SELECT 
+            dep2_ID
+        FROM
+            Parent_Relation
+        WHERE
+            dep1_ID = '1000' AND parent = '1');
+
+#Query to get featured products from homepage, including their title, desc, price, and a link to their page
+SELECT 
+    title, prod_desc, prod_price, prod_link
+FROM
+    Product
+WHERE
+    prod_ID IN (SELECT 
+            prod_ID
+        FROM
+            Featured);
+            
+#Query to get all products that are keyword-related to a chosen product - in this case the "Banana"
+
+SELECT DISTINCT
+    P2.title
+FROM
+    Product AS P1
+        JOIN
+    Product_Keywords AS PK1 ON P1.prod_ID = PK1.prod_ID
+        JOIN
+    Product_Keywords AS PK2 ON PK1.keyword = PK2.keyword
+        JOIN
+    Product AS P2 ON PK2.prod_ID = P2.prod_ID
+WHERE
+    P1.title = 'banana'
+        AND P2.title != 'banana';
+        
+#Query to list all products within a certain department (in this case, Computers) along with their title, desc, price, avg rating when g
+select * from Department;
+
+
+
